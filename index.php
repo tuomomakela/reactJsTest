2c86abcbd509b2498086bcae9c6ac17987249b8d
Tuomo Mäkelä <tuomo@lyyti.fi>
2015-03-08 14:13:32 +0200

    radical changes in structure - adding is also working

--- index.php
+++ index.php
@@ -9,102 +9,109 @@
   <body>
     <div id="content"></div>
     <script type="text/jsx">
-		var ListElement = React.createClass({
-			getInitialState: function() {
-				return {
-					isEdited: false,
-					name: this.props.name
-				};
-			},
-			handleEdit: function(event) {
-				this.setState({isEdited: true});
-				console.log("start editing");
-			},
-			handleSave: function(event) {
-				// ajax to handle save
-				this.setState({name: this.refs.name.getDOMNode().value.trim()});
-				this.setState({isEdited: false});
-			},
-			handleCancel: function(event) {
-				this.setState({isEdited: false});
-			},
-			handleDelete: function(event) {
-				// ajax to handle delete
-				React.unmountComponentAtNode(this.refs.li.getDOMNode());
-				$(this.refs.li.getDOMNode()).remove();
-			},
-			render: function() {
-				var textData = this.state.isEdited
-					? <span>
-						<input type="text" defaultValue={this.state.name} ref="name" />
-						<input type="button" value="Save" onClick={this.handleSave} />
-						<input type="button" value="Cancel" onClick={this.handleCancel} />
-					  </span>
-					: <span onClick={this.handleEdit}> {this.state.name} </span>
-				return(
-					<li className="listElement" ref="li">
-						<div className="textElem">
-							{textData}
-						</div>
-						<div className="deleteElem" onClick={this.handleDelete}>
-							[x]
-						</div>
-					</li>
-				);
-			}
-		});
-		var CreateElement = React.createClass({
-			handleSubmit: function(event) {
-				event.preventDefault();
-				var name = this.refs.name.getDOMNode().value.trim();
-				if (!name) return;
-				// ajax to handle save
-				this.props.onNewItemSubmit({name: name});
-				this.refs.name.getDOMNode().value = '';
-			},
-			render: function() {
-				return (
-					<li className="listElement">
-						<form className="createNewForm" onSubmit={this.handleSubmit}>
-							<div className="textElem">
-								<input type="text" placeholder="New item" ref="name" />
-							</div>
-							<div className="saveBtnElem">
-								<input type="submit" value="Save" />
-							</div>
-						</form>
-					</li>
-				);
-			}
-		});
 		var List = React.createClass({
 			getInitialState: function() {
-				return {
-					data: [
-						{name: "A"},
-						{name: "B"},
-						{name: "C"}
-					]
+				var data = [
+					{name: "A"},
+					{name: "B"},
+					{name: "C"}
+				];
+				// set isEdited false for every item
+				for (var i = 0; i < data.length; ++i) {
+					data[i].isEdited = false;
 				}
+				return {
+					data: data
+				};
 			},
-			handleNewItemSubmit: function(name) {
-				this.state.data = React.addons.update(this.state.data, {$push: [{name: name}]});
-				console.log(this.state.data[this.state.data.length-1].name);
-				// problem is that this.state.data is not correct
-				// this could be avoided by getting entire data here from database
-				// in database data should be correct all the time
-				this.forceUpdate();
+			/* start editing name of item */
+			handleEdit: function(index) {
+				var data = this.state.data;
+				data[index] = React.addons.update(
+					this.state.data[index], {
+						isEdited: {$set: true}
+					}
+				);
+				this.setState(data);
+				console.log(this.state.data[index]);
+			},
+			/* save change of name */
+			handleSave: function(index) {
+				// ajax to handle save
+				var data = this.state.data;
+				data[index] = React.addons.update(
+					this.state.data[index], {
+						name: {$set: this.refs["name_"+index].getDOMNode().value.trim()},
+						isEdited: {$set: false}
+					}
+				);
+				this.setState(data);
+				console.log(this.state.data[index]);
+			},
+			/* cancel changing a name */
+			handleCancel: function(index) {
+				var data = this.state.data;
+				data[index] = React.addons.update(
+					this.state.data[index], {
+						isEdited: {$set: false}
+					}
+				);
+				this.setState(data);
+				console.log(this.state.data[index]);
+			},
+			handleDelete: function(index) {
+				// ajax to handle delete
+				var data = this.state.data;
+				data.splice(index, 1);
+				this.setState(data);
+				console.log(this.state.data);
+			},
+			handleSubmit: function(event) {
+				event.preventDefault();
+				var data = this.state.data;
+				data[data.length] = {
+					name: this.refs.newName.getDOMNode().value.trim(),
+					isEdited: false
+				}
+				// ajax to handle save
+				this.setState(data);
+				this.refs.newName.getDOMNode().value = '';
 			},
 			render: function() {
-				var itemData = this.state.data.map(function(item) {
-					return (
-						<ListElement {...item} />
-					);
-				});
 				return(
 					<ul className="list">
-						{itemData}
-						<CreateElement onNewItemSubmit={this.handleNewItemSubmit} />
+					{ 
+						this.state.data.map(function(item, i) {
+							return (
+								<li className="listElement" ref={"li_"+i}>
+									<div className="textElem">
+										{
+											item.isEdited
+												? <span>
+													<input type="text" defaultValue={item.name} ref={"name_"+i} />
+													<input type="button" value="Save" onClick={this.handleSave.bind(this,i)} />
+													<input type="button" value="Cancel" onClick={this.handleCancel.bind(this,i)} />
+												  </span>
+												: <span key={i} onClick={this.handleEdit.bind(this,i)}> {item.name} </span>
+										}
+									</div>
+									<div className="deleteElem" onClick={this.handleDelete.bind(this,i)}>
+										[x]
+									</div>
+								</li>
+							);
+						}, this)
+					}
+						<li className="listElement">
+							<form className="createNewForm" onSubmit={this.handleSubmit}>
+								<div className="textElem">
+									<input type="text" placeholder="New item" ref="newName" />
+								</div>
+								<div className="saveBtnElem">
+									<input type="submit" value="Save" />
+								</div>
+							</form>
+						</li>
 					</ul>
 				);
 			}
